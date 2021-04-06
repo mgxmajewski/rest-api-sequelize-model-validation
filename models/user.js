8653843951788cccd728f023fd46d03f6eca343f
mgxmajewski <60788689+mgxmajewski@users.noreply.github.com>
2021-04-06 23:20:14 +0200

    Add validation custom validation messages

--- models/user.js
+++ models/user.js
@@ -6,19 +6,39 @@ module.exports = (sequelize) => {
   User.init({
     name: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A name is required'
+        }
+      }
     },
     email: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A email is required'
+        }
+      }
     },
     birthday: {
       type: DataTypes.DATEONLY,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A birthday is required'
+        }
+      }
     },
     password: {
       type: DataTypes.STRING,
-      allowNull: false
+      allowNull: false,
+      validate: {
+        notNull: {
+          msg: 'A password is required'
+        }
+      }
     }
   }, { sequelize });
 
